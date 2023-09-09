6ec4b2a6ad7f0061556983bb1afaa5ceb120a208
ngotruonganh <ngotruonganh1901@gmail.com>
2023-09-09 23:22:53 +0700

    login

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -2,3 +2,5 @@ import http from '../utils/http.ts'
 import { AuthResponse } from '../types/auth.type.ts'
 
 export const registerAccount = (body: { email: string; password: string }) => http.post<AuthResponse>('/register', body)
+
+export const loginAccount = (body: { email: string; password: string }) => http.post<AuthResponse>('/login', body)
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,32 +1,70 @@
-import { Link } from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import Input from "../../components/Input";
+import {useForm} from "react-hook-form";
+import {getRules} from "../../utils/rules.ts";
+import {useMutation} from "@tanstack/react-query";
+import {loginAccount} from "../../apis/auth.api.ts";
+import {toast} from "react-toastify";
+
+interface FormData {
+  email: string
+  password: string
+}
 
 export default function Login() {
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    formState: { errors }
+  } = useForm<FormData>()
+  const rules = getRules(getValues)
+  const loginAccountMutation = useMutation({
+    mutationFn: (body: FormData) => loginAccount(body)
+  })
+  const onSubmit = handleSubmit((data) => {
+    const body = data
+    loginAccountMutation.mutate(body, {
+      onSuccess: (data) => {
+        toast.success(data.data.message)
+        navigate('/')
+      },
+      onError: () => {
+        toast.error('Email hoặc password không đúng')
+      }
+    })
+  })
+  const value = watch()
+  console.log(value)
+
   return (
     <div className='bg-orange'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng nhập</div>
-              <div className='mt-8'>
-                <input
-                  type='email'
-                  name='email'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                  placeholder='Email'
-                />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-              </div>
-              <div className='mt-3'>
-                <input
-                  type='password'
-                  name='password'
-                  autoComplete='on'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                  placeholder='Password'
-                />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-              </div>
+              <Input
+                name='email'
+                register={register}
+                type='email'
+                className='mt-8'
+                errorMessage={errors.email?.message}
+                placeholder='Email'
+                rules={rules.email}
+              />
+              <Input
+                name='password'
+                register={register}
+                type='password'
+                className='mt-2'
+                errorMessage={errors.password?.message}
+                placeholder='Password'
+                autoComplete='on'
+                rules={rules.password}
+              />
               <div className='mt-3'>
                 <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
                   Đăng nhập
