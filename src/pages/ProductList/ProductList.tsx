a75017dbd799b19b087c40a27baf2be009448278
ngotruonganh <ngotruonganh1901@gmail.com>
2023-09-12 03:56:57 +0700

    sort product feature

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -17,7 +17,7 @@ export default function ProductList() {
   const queryConfig: QueryConfig = omitBy(
     {
       page: queryParams.page || '1',
-      limit: queryParams.limit || 5,
+      limit: queryParams.limit || 10,
       sort_by: queryParams.sort_by,
       exclude: queryParams.exclude,
       name: queryParams.name,
@@ -36,7 +36,6 @@ export default function ProductList() {
     },
     keepPreviousData: true
   })
-  console.log(data)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -46,7 +45,7 @@ export default function ProductList() {
               <AsideFilter />
             </div>
             <div className='col-span-9'>
-              <SortProductList />
+              <SortProductList queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
               <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                 {data.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
